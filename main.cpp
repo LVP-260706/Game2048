6e2cb4c4df8d2fc8aafdca3b59b14602fdc05dd1
Lâm Việt Phúc <lamvietphuc26072006@gmail.com>
2025-03-24 00:09:45 +0700

    DemoGrid

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include "Image.h"
 using namespace std;
 const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
+
+void drawGrid(SDL_Renderer* renderer, int gridSize, int cellSize)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (int i = 0; i <= gridSize; i++)
+    {
+        SDL_RenderDrawLine(renderer, 0, i * cellSize, cellSize * gridSize, i * cellSize);
+        SDL_RenderDrawLine(renderer, i * cellSize, 0, i * cellSize, gridSize * cellSize);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     Window window("Game 2048", SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -16,8 +27,10 @@ int main(int argc, char* argv[])
             if (event.type == SDL_QUIT) running = false;
         }
         SDL_RenderClear(renderer);
-        image.renderer(100, 50, 600, 400);
+        image.renderer(0, 0, 800, 600);
+        drawGrid(renderer, 4, 100);
         SDL_RenderPresent(renderer);
+
     }
     return 0;
 }
