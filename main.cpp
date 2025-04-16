087a29de63f17a63c29c96492b5977bf5f51667a
Lâm Việt Phúc <lamvietphuc26072006@gmail.com>
2025-04-17 00:31:33 +0700

    Start_Button

--- main.cpp
+++ main.cpp
@@ -25,7 +25,11 @@ int main(int argc, char* argv[])
 {
     Window window("Game 2048", SCREEN_WIDTH, SCREEN_HEIGHT);
     SDL_Renderer* renderer = window.getRenderer();
-    Image image("Image/2048_mainImage.png", renderer);
+
+    Image mainBackground("Image/2048_mainImage.png", renderer);
+    Image startButton("Image/start.png", renderer);
+    SDL_Rect startButtonRect = {SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT - 180, 200, 80};
+
     Image scoreBox("Image/score.png", renderer);
     Image bestBox("Image/best.png", renderer);
 
@@ -39,8 +43,7 @@ int main(int argc, char* argv[])
     bool running = true;
     bool gameOver = false;
     bool win = false;
-    spawnRandomNumber();
-    spawnRandomNumber();
+    bool startMenu = true;
 
     SDL_Event event;
     while (running)
@@ -48,48 +51,65 @@ int main(int argc, char* argv[])
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT) running = false;
-            else if (event.type == SDL_KEYDOWN)
+            if (startMenu && event.type == SDL_MOUSEBUTTONDOWN)
+            {
+                int mouseX = event.button.x;
+                int mouseY = event.button.y;
+                if (mouseX >= startButtonRect.x && mouseX <= startButtonRect.x + startButtonRect.w &&
+                        mouseY >= startButtonRect.y && mouseY <= startButtonRect.y + startButtonRect.h)
+                    {
+                        startMenu = false;
+                        spawnRandomNumber();
+                        spawnRandomNumber();
+                    }
+            }
+            else if (!startMenu && event.type == SDL_KEYDOWN)
             {
                 handleKey(event, gameOver, win);
             }
         }
-
-        SDL_SetRenderDrawColor(renderer, 250, 248, 239, 255);
-        SDL_RenderClear(renderer);
-        image.renderer(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-        scoreBox.renderer(149, 95, 140, 80);
-        bestBox.renderer(410, 95, 140, 80);
-        renderScore(renderer, font, score, 219, 138);
-        renderScore(renderer, font, best, 481, 138);
-
-        drawBoard(renderer, SCREEN_WIDTH, SCREEN_HEIGHT, font);
-
-        if (gameOver)
+        if (startMenu)
         {
-            SDL_Color color = {255, 0, 0};
-            SDL_Surface* surface = TTF_RenderText_Solid(font, "Game Over! Press R to restart", color);
-            SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-            int textW, textH;
-            SDL_QueryTexture(texture, NULL, NULL, &textW, &textH);
-            SDL_Rect textRect = { (SCREEN_WIDTH - textW) / 2, (SCREEN_HEIGHT - textH) / 2 + 45, textW, textH };
-            SDL_RenderCopy(renderer, texture, NULL, &textRect);
-            SDL_FreeSurface(surface);
-            SDL_DestroyTexture(texture);
+            mainBackground.renderer(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            startButton.renderer(startButtonRect.x, startButtonRect.y, startButtonRect.w, startButtonRect.h);
         }
-
-        if (win)
+        else
         {
-            SDL_Color color = {0, 128, 0};
-            SDL_Surface* surface = TTF_RenderText_Solid(font, "You win! Press R to restart", color);
-            SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-            int textW, textH;
-            SDL_QueryTexture(texture, NULL, NULL, &textW, &textH);
-            SDL_Rect textRect = { (SCREEN_WIDTH - textW) / 2, (SCREEN_HEIGHT - textH) / 2 + 45, textW, textH };
-            SDL_RenderCopy(renderer, texture, NULL, &textRect);
-            SDL_FreeSurface(surface);
-            SDL_DestroyTexture(texture);
-        }
+            SDL_SetRenderDrawColor(renderer, 220, 218, 209, 255);
+            SDL_RenderClear(renderer);
+            scoreBox.renderer(149, 95, 140, 80);
+            bestBox.renderer(410, 95, 140, 80);
+            renderScore(renderer, font, score, 219, 138);
+            renderScore(renderer, font, best, 481, 138);
 
+            drawBoard(renderer, SCREEN_WIDTH, SCREEN_HEIGHT, font);
+
+            if (gameOver)
+            {
+                SDL_Color color = {255, 0, 0};
+                SDL_Surface* surface = TTF_RenderText_Solid(font, "Game Over! Press R to restart", color);
+                SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+                int textW, textH;
+                SDL_QueryTexture(texture, NULL, NULL, &textW, &textH);
+                SDL_Rect textRect = { (SCREEN_WIDTH - textW) / 2, (SCREEN_HEIGHT - textH) / 2 + 45, textW, textH };
+                SDL_RenderCopy(renderer, texture, NULL, &textRect);
+                SDL_FreeSurface(surface);
+                SDL_DestroyTexture(texture);
+            }
+
+            if (win)
+            {
+                SDL_Color color = {0, 128, 0};
+                SDL_Surface* surface = TTF_RenderText_Solid(font, "You win! Press R to restart", color);
+                SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+                int textW, textH;
+                SDL_QueryTexture(texture, NULL, NULL, &textW, &textH);
+                SDL_Rect textRect = { (SCREEN_WIDTH - textW) / 2, (SCREEN_HEIGHT - textH) / 2 + 45, textW, textH };
+                SDL_RenderCopy(renderer, texture, NULL, &textRect);
+                SDL_FreeSurface(surface);
+                SDL_DestroyTexture(texture);
+            }
+        }
         SDL_RenderPresent(renderer);
     }
 
